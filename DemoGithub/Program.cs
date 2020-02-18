d526998201d6bfaa172d6bb2592c6b2aa1088cdf
anushag1104@gmail.com <anushag1104@gmail.com>
2020-02-18 09:30:41 +0530

    changed

--- DemoGithub/Program.cs
+++ DemoGithub/Program.cs
@@ -10,7 +10,7 @@ namespace DemoGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello All");
             Console.ReadLine();
         }
     }
